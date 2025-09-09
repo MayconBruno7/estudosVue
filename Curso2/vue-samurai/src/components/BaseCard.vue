ce4823afdec7d9308a365b60b0bef77bfb3e5996
Maycon Bruno <114522993+MayconBruno7@users.noreply.github.com>
2025-09-08 21:04:53 -0300

    Inicio projeto do curso

---
+++ Curso2/vue-samurai/src/components/BaseCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <b-col md="3">
+            <b-card title="Curso de VueJS Tittle" img-src="https://picsum.photos/id/237/200/300" img-alt="Curso de Vue.JS" style="max-width: 20rem;" img-top tag="article" class="mb-2">
+                <b-card-text>
+                    Lorem ipsum dolor sit amet
+                </b-card-text>
+                <b-button variant="primary">Comprar</b-button>
+            </b-card>
+        </b-col>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCard",
+}
+</script>
---
+++ Curso2/vue-samurai/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container> 
+    <b-row align-v="center">
+      <BaseCard />
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import BaseCard from '@/components/BaseCard.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    BaseCard
+  }
+}
+</script>
